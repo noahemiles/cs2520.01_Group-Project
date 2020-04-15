cf058b58e42d0889977cbbeabefac3e77d38a204
noahemiles <noahemiles@gmail.com>
2020-04-14 17:47:16 -0700

    removed comment

--- beautifulSoupTest.py
+++ beautifulSoupTest.py
@@ -7,8 +7,6 @@ stockLabel = input("Enter Stock Label: ").upper()
 page = urllib.request.urlopen("http://www.finance.yahoo.com/quote/"+stockLabel)
 pageText = page.read()
 decodedPageText = pageText.decode("utf-8")
-#location = decodedPageText.find("<span class=")
-#print(decodedPageText[location:location + 97])
 yahooQuery = "<span class="
 find = lambda sentence, qry: sentence[sentence.find(qry):sentence.find(qry)+97] if qry in sentence else -1
 
