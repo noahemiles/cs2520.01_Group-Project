3a5cd8ed9aea1c0550adff75eedc1a61db5838f1
noahemiles <noahemiles@gmail.com>
2020-05-07 10:17:13 -0700

    annotated code

--- stockAnalytics.py
+++ stockAnalytics.py
@@ -11,38 +11,60 @@ import os
 
 class StockAnalytics:
 	def __init__(self, stockLabel, timePerPriceCheck = 30, url = "http://www.finance.yahoo.com/quote/"):
+	'''
+	Sets stock label, the time interval to check stock price, and the url of the webpage
+	'''
 		self.stockLabel = stockLabel.upper()
 		self.timePerPriceCheck = timePerPriceCheck #seconds
 		self.url = url + stockLabel.upper()
 
 	def getStockLabel(self):
+
+	'''
+	Returns the stock label 
+	'''
 		return self.stockLabel
 
 	def getUrl(self):
+	'''
+	Returns the url of the webpage
+	'''
 		return self.url
 
 	def getPage(self):
+	'''
+	Returns the webpage text of the url
+	'''
 		return requests.get(self.url).text
 
 	def getStockPrice(self):
+	'''
+	Parses the webpage for the stock price 
+	'''
 		return bsoup(self.getPage(),"html.parser").find('div', {'class':'My(6px) Pos(r) smartphone_Mt(6px)'}).find('span').get_text()
 
 	def writeCSVFile(self):
+	'''
+	Writes stock information to csv file
+	'''
 		try:
 			stockLabelFile = csv.writer(open('./csvFiles/' + self.getStockLabel() + ".csv", "a"))
 		except IOError as err:
+			#creates csvFile directory if one does not exist in the current directory
 			os.mkdir("./csvFiles")
 			stockLabelFile = csv.writer(open('./csvFiles/' + self.getStockLabel() + ".csv", "a"))
 		except Exception as ex:
 			print(ex)
 			return False
 		finally:
+			#time information
 			today = datetime.now()
 			dt_string = today.strftime("%H:%M")
 			year = today.year
 			month = today.month
 			day = today.day
 			try:
+				#file write row
 				stockLabelFile.writerow([self.getStockPrice(), dt_string, month, day, year])
 			except AttributeError as writeError:
 				return False
@@ -50,6 +72,11 @@ class StockAnalytics:
 
 
 	def collectStockPrices(self, totalTime, timeUnit = "H"):
+	'''
+	Controls how long it is to write to the csv file
+
+	Default time unit is HOURS
+	'''
 		if timeUnit.upper() is "S":
 			endTime = time.time() + totalTime
 		elif timeUnit.upper() is "M":
@@ -62,12 +89,17 @@ class StockAnalytics:
 				print("Error Writing:",self.getStockLabel(),"|",datetime.now())
 
 	def __str__(self):
+	'''
+	@Override 
+	Prints Stock Label : Stock Price
+	'''
 		return self.getStockLabel() + ": " + self.getStockPrice()
 
-def create(label):
-	return StockAnalytics(label)
 
 def dayCollection():
+	'''
+	Collects stock data from all stock labels in StockList for 7 hours by default
+	'''
 	os.system('cls' if os.name == 'nt' else 'clear')
 	print("Starting collection:", datetime.now())
 	StockList = list()
@@ -97,6 +129,8 @@ def autoStart():
 	waitThread.start()
 
 def main():
+	#manual start meaning, starting immediately 
+	#auto start meaning, starting the next day at 6am
 	if input("Manual Start = 0\nAuto Start = 1\n\tInput: ") is 0:
 		dayCollection()
 	else:
