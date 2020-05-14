593c020b1b16fccf6c5bc234483a2bf48526fce3
rjindoo <platypus2k1@gmail.com>
2020-05-13 20:48:32 -0700

    Removed lower frame from gui and updated picture

--- gui.py
+++ gui.py
@@ -17,23 +17,23 @@ def testing(stockLabel):
 	obj1 = g.liveGraph(stockLabel)
 	obj1.graph()
 
-frame = Frame(root, bg='#ccc6ff', bd=5)
+frame = Frame(root, bd=5)
 frame.place(relx=0.5, rely=0.1, relwidth=.75, relheight=.1, anchor='n')
 
-button = Button(frame, text="Show stonks", fg = 'red', command = lambda: testing(entry.get()))
+button = Button(frame, text="Show stock", fg = 'red', command = lambda: testing(entry.get()))
 button.place(relx=.75, rely=0, relwidth=.25, relheight=1)
 
 entry = Entry(frame, bg = '#80c1ff')
 entry.place(relx=0, rely=0, relwidth=.54, relheight=1)
 
-label = Label(root, text="Enter Stock Label Below")
+label = Label(root, bg = 'gray', text="Enter Stock Label Below")
 label.place(relx=.125, rely=.05, relwidth=.404, relheight=.05)
 
-lower_frame = Frame(root, bg='#ccc6ff', bd=10)
-lower_frame.place(relx=0.5, rely=.25, relwidth=.75, relheight=.6, anchor='n')
-
-lower_label = Label(lower_frame, text="Displaying Stock")
-lower_label.place(relwidth=1, relheight=1)
+#lower_frame = Frame(root, bg='#ccc6ff', bd=10)
+#lower_frame.place(relx=0.5, rely=.25, relwidth=.75, relheight=.6, anchor='n')
+#
+#lower_label = Label(lower_frame, text="Displaying Stock")
+#lower_label.place(relwidth=1, relheight=1)
 
 
 
